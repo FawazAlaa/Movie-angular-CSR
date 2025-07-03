a3ba14df1a2362f22c5153df108909fd780f21ff
Fawaz <fawazx9@hotmail.com>
2025-07-04 00:52:03 +0300

    latest commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,9 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     { path: '', loadComponent: () => import('./movies/movies').then(m => m.Movies), pathMatch: 'full' },
-    { path: 'single-movie/:id', loadComponent: () => import('./single-movie/single-movie').then(m => m.SingleMovie), pathMatch: 'full' },
-    { path: 'single-show/:id', loadComponent: () => import('./single-show/single-show').then(m => m.SingleShow), pathMatch: 'full' },
+    { path: 'single-movie/:id', loadComponent: () => import('./single-movie/single-movie').then(m => m.SingleMovie), pathMatch: 'full', data: { renderMode: 'no-prerender' } },
+    { path: 'single-show/:id', loadComponent: () => import('./single-show/single-show').then(m => m.SingleShow), pathMatch: 'full', data: { renderMode: 'no-prerender' } },
     { path: 'watchlist', loadComponent: () => import('./watchlist/watchlist').then(m => m.Watchlist), pathMatch: 'full' },
-    { path: 'searchresult/:name', loadComponent: () => import('./searchresult/searchresult').then(m => m.Searchresult), pathMatch: 'full' },
-    { path: '**', loadComponent: () => import('./not-found/not-found').then(m => m.NotFound) }
+    { path: 'searchresult/:name', loadComponent: () => import('./searchresult/searchresult').then(m => m.Searchresult), pathMatch: 'full',data: { renderMode: 'no-prerender' } },
+    { path: '**', loadComponent: () => import('./not-found/not-found').then(m => m.NotFound), data: { renderMode: 'no-prerender' } }
 ];
